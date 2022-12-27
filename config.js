f5cc966921fde4d4dac107cc47d1c33067780eca
Claudine Napili <Claudine.napili13@gmail.com>
2022-12-27 21:54:06 +0800

    done

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "780c5e36b4adc75ba77fc3054ce5d435"
+    }
+}
+
+module.exports = constants;
